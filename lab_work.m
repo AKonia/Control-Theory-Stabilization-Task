2a197b36a986e8e886aeb7891e85f2d87a8ef842
Alexandr <konakov@my.com>
2018-05-19 02:32:32 +0300

    Шикарное исполнение, теперь граница там где надо

--- lab_work.m
+++ lab_work.m
@@ -5,16 +5,14 @@ graphics_toolkit fltk;
 
 
 % Управление отрисовкой и задачей
-square_size = 5.0;
+square_size = 2.0;
 draw_lines = 1;
 
-global_a = -0.28; % a < -w^2
-global_b = -0.1; % b < 0
-global_w = 0.5; %
-global_u0 = 1.0; %
-global global_isLinearSystem = 2;
-
-
+global_a = -2; % a < -w^2
+global_b = -1; % b < 0
+global_w = 1; %
+global_u0 = 2.0; %
+global global_isLinearSystem = 1;
 
 
 function [t, y] = custom_ode(x_coord, y_coord, w, a, b, u0)
@@ -47,15 +45,17 @@ function [t, y] = custom_ode(x_coord, y_coord, w, a, b, u0)
     else
       a = 0;
       b = 0;
-      u0 = back_u0 * sign([a, b]*[p_x; p_y]);
+      u0 = back_u0 * sign([p_x, p_y]*[a; b]);
     endif  
     lab_sys_linear_case = 0;
-    if(global_isLinearSystem)
+    if(global_isLinearSystem == 1)
       lab_sys_linear_case = @(t,y) [y(2); (w^2)*y(1) + a*y(1) + b*y(2) + u0];
     else
       lab_sys_linear_case = @(t,y) [y(2); (w^2)*sin(y(1)) + a*y(1) + b*y(2) + u0];
     endif
-    
+    if(length([timing:calc_step:max_time]) < 2)
+    	break
+    endif
     [t, y] = ode45(lab_sys_linear_case, [timing:calc_step:max_time], [p_x, p_y]);
 
     a = back_a;
@@ -64,7 +64,7 @@ function [t, y] = custom_ode(x_coord, y_coord, w, a, b, u0)
     
     for indx = [1:1:length(y)]
       last_control = is_control_type_smart; 
-      if(abs(y(indx)*[a;b]) < u0)
+      if(abs(y(indx,:)*[a;b]) < back_u0)
         is_control_type_smart = 1;
       else
         is_control_type_smart = 0;
@@ -83,20 +83,110 @@ function [t, y] = custom_ode(x_coord, y_coord, w, a, b, u0)
   t = [0:calc_step:max_time];
   y = descision_y;
   t = t(1:length(y));
-  length(t)
-  length(y)
 endfunction
-
-
 
 
 hold on;
 axis([-square_size square_size -square_size square_size]);
 draw_curves = 1;
 if(draw_lines)
-	fi_dot = [-10:0.1:10];
-    plot(fi_dot, ( global_u0 - global_a*fi_dot)/global_b, 'linewidth', 2, 'k');
-    plot(fi_dot, (-global_u0 - global_a*fi_dot)/global_b, 'linewidth', 2, 'k');
+	fi_dot = [-1000:100:1000];
+    plot(fi_dot, ( global_u0 - global_a*fi_dot)/global_b, 'linewidth', 1, 'k');
+    plot(fi_dot, (-global_u0 - global_a*fi_dot)/global_b, 'linewidth', 1, 'k');
+    
+    u0 = global_u0;
+    a = global_a;
+    b = global_b;
+    w = global_w;
+    calc_step = 0.01;
+    
+    root_x_right = -(b*u0*w^2 + a*b*u0)/(b^2*w^2 + (a - 1)*b^2 - 1);
+    root_x_left = (b*u0*w^2 + a*b*u0)/(b^2*w^2 + (a - 1)*b^2 - 1);
+    
+    root_y = @(x)(-u0/a - b/a*x);
+    root_y_right = root_y(root_x_right);
+    root_y = @(x)(u0/a - b/a*x);
+    root_y_left = root_y(root_x_left);
+    
+    plot([root_x_right], [root_y_right], '*');
+    plot([root_x_left], [root_y_left], '*');
+    
+    u0 = 0;
+    if(global_isLinearSystem == 1)
+      lab_sys_linear_case = @(t,y) [y(2); (w^2)*y(1) + a*y(1) + b*y(2) + u0];
+    else
+      lab_sys_linear_case = @(t,y) [y(2); (w^2)*sin(y(1)) + a*y(1) + b*y(2) + u0];
+    endif
+    
+    
+    
+    
+    
+    [t, y] = ode45(lab_sys_linear_case, [0:-calc_step:-10], [root_x_right, root_y_right]);
+    desc = [];
+    next_point = 0;
+    last_t = 0;
+    for indx = [1:1:length(y)]
+    	if(abs(y(indx, :)*[a;b]) < global_u0)
+    		desc = y(1:indx, :);
+    		next_point = y(indx, :);
+    		last_t = t(indx);
+    		break;
+    	endif
+    endfor
+    
+	[t, y] = ode45(lab_sys_linear_case, [last_t:-calc_step:-10], next_point);    
+    desc = [];
+    for indx = [1:1:length(y)]
+    	if(abs(y(indx, :)*[a;b]) > global_u0)
+    		desc = [desc, y(1:indx, :)];
+    		break;
+    	endif
+    endfor
+    plot(desc(:,1), desc(:,2), 'linewidth', 2, 'k');
+    
+    [t, y] = ode45(lab_sys_linear_case, [0:-calc_step:-10], [root_x_left, root_y_left]);
+    for indx = [1:1:length(y)]
+    	if(abs(y(indx, :)*[a;b]) < global_u0)
+    		desc = y(1:indx, :);
+    		next_point = y(indx, :);
+    		break;
+    	endif
+    endfor
+    
+    [t, y] = ode45(lab_sys_linear_case, [last_t:-calc_step:-10], next_point);    
+    desc = [];
+    for indx = [1:1:length(y)]
+    	if(abs(y(indx, :)*[a;b]) > global_u0)
+    		desc = [desc, y(1:indx, :)];
+    		break;
+    	endif
+    endfor
+   
+    plot(desc(:,1), desc(:,2), 'linewidth', 2, 'k');
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if(global_isLinearSystem == 0)
+    	fi_dot = [-1:0.1:1];
+    	[seps, eigens] = eig([0, 1; global_w^2, 0]);
+    	plot(fi_dot - global_u0/(global_w^2), fi_dot*(seps(:,2)(2)/seps(:,2)(1)),'linewidth', 1, 'k');
+    	plot(fi_dot - global_u0/(global_w^2), fi_dot*(seps(:,1)(2)/seps(:,1)(1)),'linewidth', 1, 'k');
+    	[seps, eigens] = eig([0, 1; global_w^2, 0]);
+    	plot(fi_dot + global_u0/(global_w^2), fi_dot*(seps(:,2)(2)/seps(:,2)(1)),'linewidth', 1, 'k');
+    	plot(fi_dot + global_u0/(global_w^2), fi_dot*(seps(:,1)(2)/seps(:,1)(1)),'linewidth', 1, 'k');
+    endif
 endif
 while(draw_curves == 1)
   [x_coord, y_coord] = ginput(1);
